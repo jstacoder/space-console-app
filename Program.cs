f65542ce018179d42fc375af53b7342b902b1879
kyle roux <jstacoder@gmail.com>
2024-01-07 17:48:31 -0800

    added boilerplate copde for space api to program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using JetBrains.Space.Client;
+using JetBrains.Space.Common;
+
+// url of space instance
+var url = new Uri("https://tzp-software.jetbrains.space");
+// access token 
+// read from .token file
+var token = ''
+var authTokens = new AuthenticationTokens(token);
+var connection = new BearerTokenConnection(url, authTokens);
+
+var chatClient = new ChatClient(connection);
+await chatClient.Messages.SendMessageAsync(
+        recipient: MessageRecipient.Member(ProfileIdentifier.Username("jstacoder")),
+        content: new ChatMessageText("Wazzzzz aaappppppp"));
